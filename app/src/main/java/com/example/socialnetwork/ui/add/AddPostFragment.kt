ea10421cb71e2fee3e1521f08838265a062693f0
adilbaev-ruslan <adilbaev-ruslan@umail.uz>
2021-01-16 23:57:05 +0500

    add username post

--- app/src/main/java/com/example/socialnetwork/ui/add/AddPostFragment.kt
+++ app/src/main/java/com/example/socialnetwork/ui/add/AddPostFragment.kt
@@ -1,6 +1,7 @@
 package com.example.socialnetwork.ui.add
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import com.example.socialnetwork.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_post_add.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class AddPostFragment: Fragment(R.layout.fragment_post_add) {
 
@@ -16,13 +18,14 @@ class AddPostFragment: Fragment(R.layout.fragment_post_add) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showData()
         postSaveButton.setOnClickListener {
             showPostLoading(true)
             val map: MutableMap<String, Any> = mutableMapOf()
             map["theme"] = etTheme.text.toString()
             map["text"] = etText.text.toString()
             map["userId"] = mAuth.currentUser?.uid!!
-            map["username"] = getUserName()
+            map["username"] = etUsername2.text.toString()
             map["like"] = 0
             map["dislike"] = 0
             map["comments"] = arrayListOf<String>()
@@ -38,19 +41,17 @@ class AddPostFragment: Fragment(R.layout.fragment_post_add) {
         }
     }
 
+    private fun showData() {
+        db.collection("users").document(mAuth.currentUser!!.uid).get()
+                .addOnSuccessListener {
+                    etUsername2.setText(it.get("username").toString())
+                }
+    }
+
     private fun showPostLoading(isLoading: Boolean) {
         postLoading.visibility = if (isLoading) View.VISIBLE else View.GONE
         etTheme.isEnabled = !isLoading
         etText.isEnabled = !isLoading
         postSaveButton.isEnabled = !isLoading
-    }
-
-    private fun getUserName(): String{
-        var usernameDB: String = ""
-        db.collection("users").document(mAuth.currentUser!!.uid).get()
-            .addOnSuccessListener {
-                usernameDB = it.get("username").toString()
-            }
-        return usernameDB
     }
 }
